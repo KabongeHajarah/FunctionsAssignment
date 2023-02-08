1186ea97c0c14cda09309deddb40211aa3138e11
KaboneHajarah <Kabongehajarah@gmail.com>
2023-02-08 14:02:43 +0300

    Functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+greeting("Ada")
+remainder(43,3)
+    var answer= remainder(43,3)
+    println(answer)
+    sum(4,12,10,1234)
+    var addition = sum(4,12,10,1234)
+    println(addition)
+    mySelf()
+
+}
+fun greeting  (name:String){
+
+    println( "Hello $name " )
+
+}
+fun remainder (num1: Int , num2: Int): Int{
+    var remainder = num1 % num2
+    return remainder
+
+}
+fun sum (num3:Int, num4: Int, num5:Int, num6: Int): Int{
+    var sum = num3 + num4 + num5 + num6
+    return sum
+}
+fun mySelf(){
+  var interestingFactAbtMe = ( "I love charity and giving back to the society")
+println(interestingFactAbtMe)
+
 }
